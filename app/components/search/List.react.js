0464c5495893b5808d69f78a4909112e0135ba4a
Fernando Yara Quesada <fernandoyara@keos.co>
2019-02-28 14:09:32 -0500

    Autocompletar

--- app/components/search/List.react.js
+++ app/components/search/List.react.js
@@ -1,5 +1,8 @@
 // import dependencies
 import React from 'react';
+import {connect} from 'react-redux';
+import Is from 'is_js';
+import _ from 'lodash';
 
 // import search
 class List extends React.Component {
@@ -14,25 +17,60 @@ class List extends React.Component {
     }
   }
 
-  /* handle change*/
-  onClick(data){
+  /* handle change */
+  onClick(data) {
     this.props.onClick(data);
     this.setState({active: false})
   }
+
+  /* node */
+  _nodes() {
+    /* nodes */
+    var _data = this.props.films;
+    var term = this.props.term.toLowerCase();
+
+    /* films */
+    var _node = Object.keys(_data).map((key, i) => {
+      // data
+      var {
+        title
+      } = _data[key];
+      if (title.toLowerCase().includes(term)) {
+        // return
+        return <span className="_item-complete" onClick={this.onClick.bind(this, title)} key={i}>{title}</span>;
+      } else {
+        return null;
+      }
+    });
+
+    /* return */
+    var _node = _.compact(_node);
+
+    if (Is.not.empty(_node)) {
+      return _node;
+    } else {
+      return <span className="_item-complete">Sorry, nothing to show :C</span>;
+    }
+
+  }
+
   // parent
-  render(){
-    var _active = this.state.active? 'is-active': '';
+  render() {
+    var term = this.props.term.toLowerCase();
+    var _active = (this.props.active && term.length >= 2)
+      ? 'is-active'
+      : '';
     // return
-    return (
-      <div className={"_list-autocomplete "+ _active}>
-        <span className="_item-complete" onClick={this.onClick.bind(this, 'List 1')}>List 1</span>
-        <span className="_item-complete" onClick={this.onClick.bind(this, 'List 2')}>List 2</span>
-        <span className="_item-complete" onClick={this.onClick.bind(this, 'List 3')}>List 3</span>
-        <span className="_item-complete" onClick={this.onClick.bind(this, 'List 4')}>List 4</span>
-      </div>
-    )
+    return (<div className={"_list-autocomplete " + _active}>
+      {this._nodes()}
+    </div>)
   }
 
 }
+
+/* store */
+var _staToProps = (state) => {
+  return {films: state.main.search.films}
+}
 //export
-export default List;
+export default connect(_staToProps)(List);
--- app/components/search/Searcher.react.js
+++ app/components/search/Searcher.react.js
@@ -32,17 +32,35 @@ class Searcher extends React.Component {
     this.setState({look: data})
   }
 
+  /* on focus */
+  onFocus(){
+    this.setState({active: true})
+  }
+
+  /* on blur */
+  onBlur(){
+    this.setState({active: false})
+  }
+
   // return
   render() {
     return (<div className="_in-searcher">
       <div className="field">
         <div className="control">
-          <input className="input _in-text-int" type="text" placeholder="lookin' for?" name="look" value={this.state.look} onChange={this.onChange.bind(this)}/>
+          <input className="input _in-text-int"
+             type="text"
+             placeholder="lookin' for?"
+             name="look"
+             value={this.state.look}
+             onChange={this.onChange.bind(this)}
+             onFocus={this.onFocus.bind(this)}
+             onBlur={this.onBlur.bind(this)}
+           />
           <button className="_button-search"><i className="fas fa-search"></i></button>
         </div>
       </div>
 
-      <List term={this.state.look} onClick={this.onClickAutocomplete.bind(this)}/>
+      <List term={this.state.look} onClick={this.onClickAutocomplete.bind(this)} active={this.state.active}/>
     </div>)
   }
 }
